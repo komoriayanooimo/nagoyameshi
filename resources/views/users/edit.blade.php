7e495867bba412d851105917e8e850f7f7df17ce
komoriayanooimo <ayano.ak7@icloud.com>
2023-12-25 14:12:32 +0900

    user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,7 +14,17 @@
         <div class="h">
         <a  href="http://localhost/nagoyameshi/public/">戻る</a><br>
         </div>
-        <h2 id="k">利用規約aaaa</h2>
+        <h2 id="k">会員情報編集</h2>
+        <div class="kk">
+          <label>氏名</label>
+          <input type="text" class="name" name="name" required="name" placeholder="小森綾乃"><br>
+          <label>メールアドレス</label>
+          <input type="text" class="email" name="name" required="email" placeholder="samurai@icloud.com"><br>
+          <label>電話番号</label>
+          <input type="text" class="tel" name="name" required="tel" placeholder="000-0000-0000"><br>
+        </div>
+        <button type="submit" class="btn edit">編集</button>
+
         <nav>
             <a href="http://localhost/nagoyameshi/public/company">会社概要</a>
             <a href="http://localhost/nagoyameshi/public/home">マイページ</a>
